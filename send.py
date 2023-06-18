75ea235909addb7449a37e578315af23d79fb0b0
Nicholas Taylor <t@Ylornico.la>
2023-06-18 16:42:44 -0400

    Added .env

--- send.py
+++ send.py
@@ -1,22 +1,29 @@
+import os
 import smtplib
 import config
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from dotenv import load_dotenv, find_dotenv
 
-def deploy(fromAddr, toAddr, subjTxt, msgTxt):
+def deploy(from_addr, to_addr, subj_txt, msg_txt):
     msg = MIMEMultipart()
-    msg['From'] = fromAddr
-    msg['To'] = toAddr
-    msg['Subject'] = subjTxt
-    message = msgTxt
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = subj_txt
+    message = msg_txt
     msg.attach(MIMEText(message, 'html'))
-    server = smtplib.SMTP(config.HOST_NAME, config.PORT_NUM)
-    server.login(config.USER, config.PASS)
+    server = smtplib.SMTP(host_name, port_num)
+    server.login(user, password)
     server.sendmail(
-        fromAddr,
-        toAddr,
+        from_addr,
+        to_addr,
         msg.as_string()
     )
     server.quit()
 
+def get_cred():
+    load_dotenv(find_dotenv())
+    return os.getenv('HOST_NAME'), os.getenv('PORT_NUM'), os.getenv('USER'), os.getenv('PASS')
+
+host_name, port_num, user, password = get_cred()
 deploy(config.FROM_ADDR, config.TO_ADDR, config.SUBL_TEXT, config.MSG_TEXT)
